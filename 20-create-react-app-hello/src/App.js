61bc41d85685e1afd4e118245a0b069432884153
pacofeng <pacofeng86@gmail.com>
2022-12-07 22:31:51 -0800

    两种定义组件的方法-组件名和index

---
+++ 20-create-react-app-hello/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import Hello from './components/Hello/Hello';
+import Welcome from './components/Welcome';
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Hello />
+        <Welcome />
+      </>
+    );
+  }
+}
---
+++ 20-create-react-app-hello/src/components/Welcome/index.js
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import './index.css';
+
+export default class Welcome extends Component {
+  render() {
+    return <div class='welcome'>Welcome to React!!</div>;
+  }
+}
