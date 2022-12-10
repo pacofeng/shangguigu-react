95cadea04d8acadc83207568f7edfe7a5c7ba092
pacofeng <pacofeng86@gmail.com>
2022-12-09 21:35:52 -0800

    向路由组件传递参数state

---
+++ 35-react-router-dom-5-nested-routes-with-state/src/pages/Home/Message/Deatil/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+const detailData = [
+  { id: '1', content: 'content 1' },
+  { id: '2', content: 'content 2' },
+  { id: '3', content: 'content 3' },
+];
+
+export default class Detail extends Component {
+  render() {
+    const { id, title } = this.props.location.state;
+    const messageDetail = detailData.find((detail) => detail.id === id) || {};
+    return (
+      <div>
+        <div>ID: {id}</div>
+        <div>TITLE: {title}</div>
+        <div>CONTENT: {messageDetail.content}</div>
+      </div>
+    );
+  }
+}
---
+++ 35-react-router-dom-5-nested-routes-with-state/src/pages/Home/Message/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+import Detail from './Deatil';
+
+export default class Message extends Component {
+  state = {
+    messgaes: [
+      { id: '1', title: 'message 1' },
+      { id: '2', title: 'message 2' },
+      { id: '3', title: 'message 3' },
+    ],
+  };
+  render() {
+    return (
+      <>
+        <ul>
+          {this.state.messgaes.map((message) => (
+            <Link
+              key={message.id}
+              to={{
+                pathname: '/home/message/detail',
+                state: {
+                  id: message.id,
+                  title: message.title,
+                },
+              }}
+            >
+              {message.title}
+            </Link>
+          ))}
+        </ul>
+        <hr />
+        <Route path='/home/message/detail' component={Detail} />
+      </>
+    );
+  }
+}
