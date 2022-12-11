8c31b9edff0c671ad29e31c9ea1dc7b3a9c554f1
pacofeng <pacofeng86@gmail.com>
2022-12-11 03:24:00 -0800

    react-redux优化

---
+++ 44-redux-react-redux-improvement/src/redux/count_action.js
@@ -0,0 +1,14 @@
+import { INCREMENT, DECREMENT } from './constant';
+
+// 同步action： 返回对象
+export const createIncrementAction = (data) => ({ type: INCREMENT, data });
+export const createDecrementAction = (data) => ({ type: DECREMENT, data });
+
+// 移步action：返回函数, 异步action中会调用同步action
+export const createIncrementAsyncAction = (data, time) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(createIncrementAction(data));
+    }, time);
+  };
+};
