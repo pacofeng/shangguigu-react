63dd55141ebb01e4caf42676265db4795faf9a0e
pacofeng <pacofeng86@gmail.com>
2022-12-14 03:41:26 -0800

    NavLink高亮

---
+++ 59-react-router-dom-6-highlight-custom-active/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink, Route, Routes, Navigate } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+
+const App = () => {
+  const getActiveClassName = ({ isActive }) => {
+    return isActive ? 'list-group-item haha active' : 'list-group-item';
+  };
+
+  return (
+    <div>
+      <div className='row'>
+        <div className='col-xs-offset-2 col-xs-8'>
+          <div className='page-header'>
+            <h2>React Router Demo</h2>
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-xs-2 col-xs-offset-2'>
+          <div className='list-group'>
+            <NavLink className={getActiveClassName} to='/about'>
+              About
+            </NavLink>
+            <NavLink className={getActiveClassName} to='/home'>
+              Home
+            </NavLink>
+          </div>
+        </div>
+        <div className='col-xs-6'>
+          <div className='panel'>
+            <div className='panel-body'>
+              <Routes>
+                <Route path='/about' caseSensitive element={<About />} />
+                <Route path='/home' element={<Home />} />
+                <Route path='/' element={<Navigate to='/home' />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
